b6786fe0a2013bdbcb15ddce83f42519a64829a0
CodeTurtle <48864709+codeturtle00@users.noreply.github.com>
2021-02-24 23:20:18 -0800

    initial upload of welcomepage

---
+++ frontend-web/src/WelcomePage/WelcomePage.js
@@ -0,0 +1,39 @@
+import './WelcomePage.css';
+import blob from '../assets/blob.svg';
+import threeCharacters from '../assets/3characters.svg'
+
+function WelcomePage() {
+  return (
+    <div id={"background"}>
+      <div id={"leftSide"}>
+        <div id={"welcomeText"}>
+          <h1 className="welcomeTitle">
+            Welcome to Spendr
+          </h1>
+          <h3 className="welcomeSubTitle">
+            Track your spendings <br/>
+            Share your cops
+          </h3>
+        </div>
+      </div>
+
+      <div>
+        <img src={blob} id={'blob'}/>
+        <img src={threeCharacters} id={'threeCharacters'}/>
+      </div>
+
+      <div id={'rightSide'}>
+        <div id={'loginCard'}>
+          <form>
+            <input type="text" name="username" placeholder={"Username"}></input>
+            <input type="password" name="password" placeholder={"Password"}></input>
+            <input type={"submit"} id={"loginBtn"} value={"Log In"}/>
+          </form>
+          <button id={"createAccBtn"}>Create New Account</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default WelcomePage;
