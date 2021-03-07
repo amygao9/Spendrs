0043ebc83f7fab8ec65e3b187d99d28677bae74c
Alex Shih <alex.alex.shih@gmail.com>
2021-03-07 16:52:02 -0500

    added pie graph spending

--- frontend-web/src/Components/Analytics/Analytics.js
+++ frontend-web/src/Components/Analytics/Analytics.js
@@ -2,19 +2,21 @@ import React from "react";
 import Navbar from "../Navbar/Navbar";
 import '../../styles/home.css';
 import { userLinks } from "../../constants";
+import SpendingPieGraph from "./SpendingPieGraph";
 
-const analStatistics = {
+const stats = {
   "budget": 300,
   "daySpending": 21.5,
   "monthSpending": 125.5,
   "yearSpending": 125.5,
+  "spendingCategories": {"food": 469, "misc": 397, "tech": 1260, "games": 220, "valorant skins": 45}
 }
 
 function Analytics(props) {
   return (
     <div className='home'>
       <Navbar links={userLinks} />
-      anal
+      <SpendingPieGraph stats={stats.spendingCategories} />
     </div>
   );
 }
