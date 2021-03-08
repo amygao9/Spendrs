87d9e765457907652f50a46d98ee5b2cda52db3c
Ray <pengray4@gmail.com>
2021-03-07 19:49:12 -0800

    Update navbar

--- frontend-web/src/App.js
+++ frontend-web/src/App.js
@@ -6,22 +6,26 @@ import history from "./config/History";
 import WelcomePage from "./Components/WelcomePage/WelcomePage";
 import Navbar from "./Components/Navbar/Navbar";
 import Home from "./Components/Dashboard/Home";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
 function App() {
   const siteLinks = {
-    "about": "About",
-    "services": "Services",
-    "contact": "Contact",
-    "signup": "Sign Up"
-  }
+    dashboard: "Feed",
+    analytics: "Analytics",
+    profile: "Profile",
+    settings: "Settings",
+  };
   return (
     <Router history={history}>
       <div className="app">
         <Switch>
           <Route exact path="/" component={WelcomePage} />
-          <Route exact path="/navbar/" component={() => (<Navbar links={siteLinks} />)} />
+          <Route
+            exact
+            path="/navbar/"
+            component={() => <Navbar links={siteLinks} />}
+          />
           <Route exact path="/dashboard" component={Home} />
         </Switch>
       </div>
