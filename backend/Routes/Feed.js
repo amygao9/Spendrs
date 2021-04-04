080d2147b494b634d18d87000e69b5d4ae97c9a7
Ray <pengray4@gmail.com>
2021-04-04 15:09:46 -0700

    Add news feed

--- backend/Routes/Feed.js
+++ backend/Routes/Feed.js
@@ -1,13 +1,18 @@
 const express = require("express");
 const { User } = require("../Database/Models/user");
+const { Post } = require("../Database/Models/post");
 
 const router = express.Router();
 
 /*
  * gets the news feed for the user starting at <date>
  *
- * date: isoString [optional] defaults to time when called if not specified
+ * date: [optional] isoString defaults to time when called if not specified
+ * num: [optional] the max num of news feed elements to pull default 50
  *
+ * return values:
+ * date: isoString for the last post
+ * posts: array of posts
  */
 router.get("/newsfeed", async (req, res) => {
   try {
@@ -22,8 +27,22 @@ router.get("/newsfeed", async (req, res) => {
       return;
     }
 
-    console.log(req.user);
-    res.send("OK");
+    console.log(timeStamp.toISOString());
+
+    const limit = req.body.num === undefined ? 50 : req.body.num;
+
+    const user = await User.findById(req.user.id);
+
+    const posts = await Post.find({
+      user: { $in: user.following },
+      createdAt: { $gte: timeStamp },
+    })
+      .sort({
+        createdAt: 1,
+      })
+      .limit(limit);
+
+    res.send(posts);
   } catch (err) {
     res.status(500).send({ err: err });
   }
