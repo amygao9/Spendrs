13aac66d5afeeba676728c01265171534ad86193
CodeTurtle <48864709+codeturtle00@users.noreply.github.com>
2021-04-08 04:07:42 -0400

    no edit if not logged in

--- frontend-web/src/Components/Profile/ProfileDescription.js
+++ frontend-web/src/Components/Profile/ProfileDescription.js
@@ -1,7 +1,7 @@
 import React, {useState} from "react";
 import '../../styles/home.css';
 import '../../styles/profile.css';
-import {Button, Container} from "react-bootstrap";
+import {Container} from "react-bootstrap";
 import ProfileStats from "./ProfileStats";
 import ProfileInfo from "./ProfileInfo";
 import ProfilePicture from "./ProfilePicture";
@@ -9,16 +9,17 @@ import {AiFillEdit} from "react-icons/all";
 
 function ProfileDescription({ user, isAdmin, loggedIn = true}) {
 
-  const [editable, setEditable] = useState(true);
+  const [editable, setEditable] = useState(false);
 
 
   return (
     <div className="profileContainer shadowMedium">
       <ProfilePicture editable={editable} user={user}/>
-      <div className={"editProfileButton"} onClick={() => setEditable(!editable)}>
-        <AiFillEdit size={"1.5em"} color={"lightgrey"} title={"Toggle Edit Profile"}/>
-        {/*<span className="tooltipBlack">Toggle Edit Profile Mode</span>*/}
-      </div>
+      {loggedIn?
+        <div className={"editProfileButton"} onClick={() => setEditable(!editable)}>
+          <AiFillEdit size={"1.5em"} color={"lightgrey"} title={"Toggle Edit Profile"}/>
+          {/*<span className="tooltipBlack">Toggle Edit Profile Mode</span>*/}
+        </div> : <></>}
 
       <Container>
         <ProfileStats user={user} isAdmin = {isAdmin}/>
