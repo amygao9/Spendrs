282a7e9d03d0856cfa1e64c1c47c5e47b69e3335
Ray <pengray4@gmail.com>
2021-04-09 12:36:15 -0700

    Search no longer returns admin users

--- backend/Routes/users.js
+++ backend/Routes/users.js
@@ -135,6 +135,7 @@ router.get("/findUser/:search", async (req, res) => {
   try {
     const users = await User.find({
       username: { $regex: `.*${req.params.search}.*` },
+      admin: false,
     }).limit(3);
     res.send(users.map((val) => val.username));
   } catch (err) {
@@ -242,8 +243,8 @@ router.get("/profile/:username", async (req, res) => {
   try {
     const user = await User.findOne({ username: req.params.username })
       .populate("posts")
-      .populate('followers', 'name username')
-      .populate('following', 'name username');
+      .populate("followers", "name username")
+      .populate("following", "name username");
     if (!user) {
       res.status(400).send({ err: "User not found" });
     }
