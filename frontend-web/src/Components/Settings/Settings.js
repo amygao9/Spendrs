791d41ec973486321817c1936d1030f1313b82e6
Amy Gao <amyy.gao@mail.utoronto.ca>
2021-04-09 13:53:08 -0400

    custom admin settings page

--- frontend-web/src/Components/Settings/Settings.js
+++ frontend-web/src/Components/Settings/Settings.js
@@ -51,15 +51,14 @@ function Settings() {
       <div>
       <div className='settingsContainer'>
         <Navbar links={adminLinks} />
-        <h3 className = "headers"> Profile Information</h3>
+        <h3 className = "headers"> Admin Account</h3>
         <div id = "users" className="list-group-item shadowSmall">
-                  <Link to={`/profile`}>
-                      <h5 className="mb-">{user.name}</h5>
-                  </Link>
+                
+                  <h5 className="mb-">{user.name}</h5>
                   <p className="mb-1">Username: @{user.username}</p>
                   <p className="mb-1">Email: {user.email}</p>
                   <p className="mb-1">Date Registered: {user.createdAt.slice(0,10)}</p>
-                  {/* <Button onClick={() => props.handleDelete(user.username)} className="btn-default" id = "deleteButton"> Delete user</Button> */}
+                  
         </div>
 
         <h3 className = "headers">Account</h3>
