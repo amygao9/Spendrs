18c7f1ccf3f7a9e1f2bc94eaec0a81a22286c07c
CodeTurtle <48864709+codeturtle00@users.noreply.github.com>
2021-04-09 15:07:33 -0400

    populating followers again

--- backend/Routes/users.js
+++ backend/Routes/users.js
@@ -240,9 +240,10 @@ router.patch("/changePassword", async (req, res) => {
 
 router.get("/profile/:username", async (req, res) => {
   try {
-    const user = await User.findOne({ username: req.params.username }).populate(
-      "posts"
-    );
+    const user = await User.findOne({ username: req.params.username })
+      .populate("posts")
+      .populate('followers', 'name username')
+      .populate('following', 'name username');
     if (!user) {
       res.status(400).send({ err: "User not found" });
     }
