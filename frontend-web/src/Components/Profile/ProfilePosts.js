15915d508ffaa23f74c71e66ccaf6ff21a85bc9f
Amy Gao <amyy.gao@mail.utoronto.ca>
2021-04-09 18:30:48 -0400

    no posts styling

--- frontend-web/src/Components/Profile/ProfilePosts.js
+++ frontend-web/src/Components/Profile/ProfilePosts.js
@@ -1,11 +1,16 @@
-import React from "react";
+import React, {useState} from "react";
 import LazyLoad from 'react-lazyload';
 import '../../styles/profile.css';
 import Post from "../Dashboard/Post";
 
 
 function ProfilePosts({ user }) {
+  const [posts, setPosts] = useState(false);
   const getFeedPosts = () => {
+    if (user.posts.length == 0) {
+      console.log(user.posts.length)
+      return <h3> No Posts have been posted. </h3>
+    }
     return user.posts.slice(0).reverse().map((post, index) => {
       return <LazyLoad key={index} height={200}>
         <div style={{borderBottom:"1px solid lightgrey", paddingTop:"5px"}}>
@@ -18,6 +23,7 @@ function ProfilePosts({ user }) {
     <div className="profilePosts shadowMedium">
       <h1 className="bold profileName"> Posts</h1>
       {getFeedPosts()}
+      
     </div>
   );
 }
